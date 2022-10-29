2a5ab741f4e46e8b0441155e8cab992f0f387dc8
tonyray2k420 <tonyray2k420@gmail.com>
2022-10-29 21:59:09 +0100

    updated

--- udagram-frontend/src/environments/environment.ts
+++ udagram-frontend/src/environments/environment.ts
@@ -5,7 +5,7 @@
 export const environment = {
   production: false,
   appName: 'Udagram',
-  apiHost: 'http://localhost:8080/api/v0'
+  apiHost: 'http://ab6e525bfe05d463797b9e25380e692d-914589147.us-east-2.elb.amazonaws.com/api/v0'
 };
 
 /*
